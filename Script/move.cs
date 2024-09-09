d8505cd39a2c1cb1258de5ff4d987a0e46a80c01
zgz <2522916105@qq.com>
2024-09-09 11:54:12 +0800

    c#测试脚本

--- Script/move.cs
+++ Script/move.cs
@@ -14,6 +14,7 @@ public class move : MonoBehaviour
         Transform trans=obj.transform;
         Vector3 position = trans.position;
         Debug.Log("****物体位置****"+position);
+        Debug.Log("123");
     }
 
     // Update is called once per frame
