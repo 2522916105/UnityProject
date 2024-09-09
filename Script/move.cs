c81db9bc9673e39df2b1f7c15cb66d59a990911a
zgz <2522916105@qq.com>
2024-09-09 11:05:03 +0800

    第一次提交

---
+++ Script/move.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("****开始测试****");
+        GameObject obj = this.gameObject;
+        string name=obj.name;
+        Debug.Log("****物体名字****"+name);
+        Transform trans=obj.transform;
+        Vector3 position = trans.position;
+        Debug.Log("****物体位置****"+position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
